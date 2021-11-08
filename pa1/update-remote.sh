68968141a046e77bcd38264b4ccd1cbe02c53119
michaelhaaf <michael.haaf@gmail.com>
2021-11-08 15:14:49 -0500

    pa1: misc script change

--- pa1/update-remote.sh
+++ pa1/update-remote.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-rsync -av --include="*/" --include="*.[h|c]" --exclude="*" ./gini5/ comp535:~/gini5/
+echo "Dry run?"
+select yn in "Yes" "No"; do
+	case $yn in 
+		Yes ) options="avn"; break;;
+		No ) options="av"; break;;
+	esac
+done
+
+rsync "-${options}" --include="*/" --include="*.[h|c]" --exclude="*" ./gini5/ comp535:~/gini5/
 
